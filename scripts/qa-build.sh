3982a00f685e7f4fb4bc06d4ccf35ad17a272106
Colin Alston <colin.alston@gmail.com>
2016-03-03 13:01:47 +0200

    Scripts should be executable

--- scripts/qa-build.sh
+++ scripts/qa-build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-$REGISTRY="qa-mesos-persistence.za.prk-host.net:5000/"
-export REGISTRY=$REGISTRY
+export REGISTRY="qa-mesos-persistence.za.prk-host.net:5000/"
 
 $REPO_DIR/scripts/docker-build.sh
